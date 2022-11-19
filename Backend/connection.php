c5cfee38baede4bad6dae87d86055a2880aee812
Sleiman Ghanem <you@example.com>
2022-11-19 17:00:49 +0200

    Back End - created registration function

--- Backend/connection.php
+++ Backend/connection.php
@@ -4,6 +4,8 @@ $db_user = "root";
 $db_pass = null;
 $db_name = "inputdb";
 
-$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+header("https://www.google.com");
 
 ?>
