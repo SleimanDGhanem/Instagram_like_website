ce330cde0ee393558967b182d8083d2438cc331f
Sleiman Ghanem <you@example.com>
2022-11-19 16:37:49 +0200

    Back End - created registration API

---
+++ Backend/register.php
@@ -0,0 +1,28 @@
+<?php
+include("connection.php");
+
+
+if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["password"])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+} else {
+  $response = [];
+  $response["success"] = false;
+  echo json_encode($response);
+  return;
+}
+
+
+$hash = password_hash(
+  $password,
+  PASSWORD_DEFAULT
+);
+
+$query = $mysqli->prepare("INSERT INTO users(name, email, password) VALUES(?,?,?)");
+$query->bind_param("sss", $name, $email, $password);
+$query->execute();
+
+$response["success"] = true;
+
+echo json_encode($response);
